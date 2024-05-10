04bbd8fe8842b3b4270497e45310bd88e0a2b322
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2024-05-10 14:57:34 +0200

    exercicio de teste de estrangeiro usando if and else

--- exercicios/ex008.js
+++
@@ -1,6 +0,0 @@
-var vel = 70
-console.log(`A velocidade do seu carro é ${vel}Km/h`)
-if (vel > 60){ //isso é uma condição simples porque não tem o else (se não)
-    console.log(`Você ultrapassou o limite de velocidade. MULTADO!`)
-}
-console.log(`Dirija sempre usando cinto de segurança!`)
---
+++ exercicios/ex009-condicaocomposta.js
@@ -0,0 +1,7 @@
+var país = 'EUA'
+if (país == 'Brasil') {
+    console.log('Você é Brasileira!')
+} else {
+    console.log('Você é Estrangeira!')
+}
+console.log(`Vivendo em: ${país}`)
