4c0f20e473f1023f0b8ce9f5452d084f2425b2e9
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2024-05-19 13:40:50 +0200

    estruturas de repeticao while & dowhile

---
+++ exercicios/ex015-while-repeticoes.js
@@ -0,0 +1,29 @@
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ // mesma coisa que c + 1
+}
+// WHILE = Estrutura de repetição com teste lógico no início: Testa -> executa -> faz loop
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
---
+++ exercicios/ex016-dowhile-repeticao.js
@@ -0,0 +1,9 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++ // mesma coisa que c + 1
+} while (c <= 6);
+
+
+
+// DO WHILE = Estrutura de repetição com teste lógico no final: executa -> testa -> faz loop
