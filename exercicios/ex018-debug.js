5568402cbed7113c0a83519ec23388fefd5af683
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2024-05-22 01:12:18 +0200

    resolvendo desafios

---
+++ exercicios/ex018-debug.js
@@ -0,0 +1,7 @@
+console.log('Vai começar...')
+for(var c = 1;c <= 4; c++){
+    console.log(c)
+}
+console.log('FIM!')
+
+ //botao do inseto e colocar a variavel em watch/inspeção para seguir o fluxo do codigo
