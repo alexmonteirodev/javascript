6901e172cc3bc72b1dd771d0fb8cafc84ca6bb40
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2024-08-16 18:06:42 +0200

    iniciando curso js origamid

---
+++ origamid/exercicios/ex001-variaveis.js
@@ -0,0 +1,19 @@
+//EXERCÍCIOS:
+
+// Declarar uma variável com o seu nome
+var nome = 'Alex'
+// Declarar uma variável com a sua idade
+var idade = 26
+// Declarar uma variável com a sua comida favorita e não atribuir valor
+var comida
+// Atribuir valor a sua comida favorita
+comida = 'pizza'
+// Declarar 5 variáveis diferentes sem valores
+var cachorro,
+    rac,
+    carro,
+    moto,
+    espanha
+
+console.log()
+
---
+++ origamid/exercicios/ex002-tipos-de-dados.js
@@ -0,0 +1,40 @@
+var nome = 'Alex' // String
+var idade = 26 // Number
+var possuiFaculdade = true // Boolean
+var time // Undefined
+var comida = null // Null
+var simbolo = Symbol() // Symbol
+var novoObjeto = {} // Object
+
+//Verificar tipo de dado:
+console.log(typeof nome)
+
+
+//Concatenação (soma)
+var gols = 1000
+var exemploConcatenacao = 'Romário fez ' + gols + ' gols'
+console.log(exemploConcatenacao)
+
+//Template String
+var exemploTemplateString = `Romário fez ${gols} gols`
+console.log(exemploTemplateString)
+
+
+//EXERCÍCIOS:
+
+// Declare uma variável contendo uma string
+var frase = 'Isso é uma String'
+// Declare uma variável contendo um número dentro de uma string
+var comNumero = 'Isso é um numero dentro de uma String: ' + 26
+var teste = `testando ${26}`
+// Declare uma variável com a sua idade
+var idade = 26
+// Declare duas variáveis, uma com seu nome e outra com seu sobrenome e some as mesmas
+var nome = 'Alex'
+var sobrenome = 'Monteiro'
+var nomeCompleto = `${nome} ${sobrenome}`
+// Coloque a seguinte frase em uma variável: It's time
+var frase1 = 'It\'s time'
+var frase2 = `It's time`
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome)
