cb99dbd7af289d9459e2382520583de4a0866200
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2024-08-18 16:57:16 +0200

    Create ex004-boolean-e-condicionais.js

---
+++ origamid/exercicios/ex004-boolean-e-condicionais.js
@@ -0,0 +1,19 @@
+//Boolean: true & false
+var possuiGraduacao = true
+var possuiDoutorado = false
+
+if (possuiGraduacao) {
+    console.log('Possui graduação')
+} else {
+    console.log('Não possui graduação')
+}
+
+// Falsy (sempre se isso acontecer o resultado será false):
+if(false)
+if(0) // ou -0
+if (NaN)
+if(null)
+if(undefined)
+if('') // ou "" ou ```
+
+// Todo o resto é truthy
