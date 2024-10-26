1f5a242c98e123102186dcad381554891e6345a6
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2024-10-26 19:20:58 +0200

    modal aulas

---
+++ origamid/exercicios/ex033-modal/aula.js
@@ -0,0 +1,11 @@
+
+                                        // Modal
+
+    // Modal é uma janela interativa. É aquela janela que abre no meio da tela quando vai fazer um login ou para se cadastrar em algo, anúncios e por aí vai.
+
+    //Geralmente o modal é criado no final do html.
+
+    //o modal tem que ser fora do grid, então englobei tudo em uma div (pq o estilo do grid estava direto no body) e passei a estilização do grid do css do body para a class grid-container
+//-------------------------------------------------------------------------------------------------------
+
+    
