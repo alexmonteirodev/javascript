2ade0ec7a970d343284038e3baa686663f38aad6
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2024-11-02 11:12:42 +0100

    Update dropdown-menu.js

--- origamid/exercicios/ex035-dropdown-menu/js/modules/dropdown-menu.js
+++ origamid/exercicios/ex035-dropdown-menu/js/modules/dropdown-menu.js
@@ -20,13 +20,22 @@ function handleClick(event) {
     event.preventDefault()
     // console.log(event)
     this.classList.add('active') //this = menu (dica, para saber o que é o this, segue ele => this>event>HandleClick>menu). toggle é a mesma coisa que add mas se clicar dnv ele remove, porém como depois tem que remover quando for add o 'eachEvent', é melhor deixar o add.
+    outsideClick(this, function () {
+        this.classList.remove('active')
+    })
 }
 
-function outsideClick(params) {
+function outsideClick(element, callback) { //f que verifica se o click foi do lado de fora (a idéia é ser uma f que verifica se está clicando no html menos no dropdown)
     const html = document.documentElement
     html.addEventListener('click', handleOutsideClick)
 
     function handleOutsideClick(event) { //f está dentro de 'outsideClick' pq só é pra funcionar quando o outsideClick for iniciado
-        console.log(event)
+        // console.log('html')
+        if (!element.contains(event.target)){//verificação pra saber se o elemento que está sendo clicado está fora do dropdown
+            callback() //esse callback está servindo de parametro (element = this; callback = function) para a a function do outsideclick que está dentro da function handleClick
+        }  
+        // console.log(element.contains(event.target)) - da true quando clica no dropdown e false quando clica fora
+        // console.log(element)
+        
     }
 }
