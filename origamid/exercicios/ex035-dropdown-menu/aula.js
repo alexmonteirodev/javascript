d73ef7703d0f56a1e611923423ead88765860d5f
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2024-11-02 12:07:07 +0100

    Update aula.js

--- origamid/exercicios/ex035-dropdown-menu/aula.js
+++ origamid/exercicios/ex035-dropdown-menu/aula.js
@@ -7,7 +7,7 @@
 
 //-------------------------------------------------------------------------------------------------------
 
-    //foi feito em css a animação do dropdown e toda a estilização e com mouse funciona tudo. O problema é para mobile que fica bugado então em js, foi feito de uma forma para que quando a pessoa clique o dropdown apareca e quando clique fora ele suma para ficar bom para os dois, desktop e mobile.
+    //foi feito em css a animação do dropdown e toda a estilização e com mouse funciona tudo. O problema é para mobile que fica bugado então em js, foi feito de uma forma para que quando a pessoa clique o dropdown apareca e quando clique fora ele suma para ficar bom para os dois, desktop e mobile. (pasta - outside-click)
 //-------------------------------------------------------------------------------------------------------
 
 //explicação:
