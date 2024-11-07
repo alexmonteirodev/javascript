99d0c5d62efdcb810834a4696b14e58edc7ed537
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2024-11-07 20:30:39 +0100

    menu-mobile

---
+++ origamid/exercicios/ex037-menu-mobile/aula.js
@@ -0,0 +1,11 @@
+
+                                        // Menu mobile
+
+    // É o menu hamburguer feito para mobile.
+
+    //para criar basta fazer add um botao antes do menu
+
+//-------------------------------------------------------------------------------------------------------
+
+    //foi adicionado um botao embaixo do nav, logo acima da ul do menu. (add atributos aria:  aria-expanded="false" aria-controls="menu" para melhor acessibilidade)
+//-------------------------------------------------------------------------------------------------------
