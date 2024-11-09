08a5f951d27b1a18d617d2819f7a25891a56d595
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2024-11-09 18:59:12 +0100

    finalizando animacao numeros

--- origamid/exercicios/ex038-animacao-numeros/aula.js
+++ origamid/exercicios/ex038-animacao-numeros/aula.js
@@ -15,5 +15,53 @@
 
     //ursos 230 / 100 = 2,3, logo temos: 2,3 + 2,3 + 2,3.... até 230
     //esquilos 12340/100 = 123,4, logo temos:  123,4 + 123,4 +  123,4.... até 230
-    
+
     //obs: para entender melhor mude o tempo do setTimeinterval para 1000
+
+
+    //explicação:
+    /*
+            export default function initAnimaNumeros(params) {
+            
+        }
+        function animaNumeros(params) {
+
+            const numeros = document.querySelectorAll('[data-numero]')
+
+
+            numeros.forEach((params) => {
+                const total = +params.innerText
+
+                const incremento = Math.floor(total/100) //floor serve para arredondar os numeros para inteiros
+
+                let start = 0
+                const timer = setInterval(() => { //repare que a arrow, fura o escopo e busca o params do forEach
+                    start += incremento //start = start + incremento
+                    params.innerText = start //para mostrar na pagina e não no console
+                    if (start > total) {
+                        params.innerText = total //pq no  if (start > total) pelo incremento o numero passa então, isso serve para ele voltar ao numero correto
+                        clearInterval(timer)
+                    }
+                }, 25 * Math.random()) //serve para cada numero terminar um pouco antes de forma randomica (fica mais organico e não parece uma maquina)
+            })
+        }
+        // a animação dos numeros não espera o scroll da pagina e para recolver isso: quando a seção mudar o atributo, para ocorrer algo, vou add um observador de mutações porque quando isso ocorrer ele dispara a animação dos numeros:
+
+        const observerTarget = document.querySelector('.numeros')
+
+        const observer = new MutationObserver(handleMutation) 
+
+        observer.observe(observerTarget, {attributes: true})
+
+        function handleMutation(mutation) {//para ativar quando tiver a mutação
+            //console.log('mudou')       //mudança no scrol-animacao.js pq ele remove ativo toda hora
+            //console.log(mutation[0]) // acessando a mutação 0 que é a unica (atributos) - console.log(mutation)
+            //console.log(mutation[0].target.classList.contains('ativo'))
+
+            if (mutation[0].target.classList.contains('ativo')) {
+                observer.disconnect() //para não resetar toda hora que der scroll
+                animaNumeros()
+
+            }
+        }
+            */
--- origamid/exercicios/ex038-animacao-numeros/js/modules/scroll-animacao.js
+++ origamid/exercicios/ex038-animacao-numeros/js/modules/scroll-animacao.js
@@ -8,11 +8,13 @@ export default function initAnimacaoScroll() {
 
         function animaScroll() {
             sections.forEach((params) => {
-                const sectionTop = params.getBoundingClientRect().top-metadeTela //- 600/60% //metodo getBounding retorna um objeto que tem as propriedades com as distancias e portanto usa-se o top
+                const sectionTop = params.getBoundingClientRect().top //- 600/60% //metodo getBounding retorna um objeto que tem as propriedades com as distancias e portanto usa-se o top
                 //console.log(sectionTop) quando o elemento passa da propriedade top ela fica negativa (-3), portanto se ela for menor que 0 é porque passou, usar o console.log comentado para ver. Porém dessa forma, se cria um espaço em branco entre uma section e outra pq tem que escrolar até ficar negativo o top da próx para ela aparecer e para corrigir isso, se subtrai alguns pixels da const nesse caso, 600. Porém 600 é um valor fixo e se a tela do client for mto grande esse valor não vai servir pq não vai carregar e se for pequena dmais vai carregar antes da hora e por isso deve se usar 60%(mais ou menos metade, 50/60%) do valor da tela (isso é feito pela const metadetela).
-                if (sectionTop < 0) {
-                    // console.log('animar')
+                const isSectionVisible = (sectionTop - metadeTela) < 0 
+                if (isSectionVisible) {
                     params.classList.add('ativo')
+                } else if (params.classList.contains('ativo')) {
+                    params.classList.remove('ativo')
                 }
             })
         }
