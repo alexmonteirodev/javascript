74ed10b7853ee78f16a21e2d662228d4418643f2
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2024-12-15 13:36:17 +0100

    fetch-bitcoin

---
+++ origamid/exercicios/ex050-animais-fetch-com-async/js/modules/fetch-animais.js
@@ -0,0 +1,32 @@
+import initAnimaNumeros from './anima-numeros.js'
+
+export default function initFetchAnimais(params) {
+    async function fetchAnimais(url) {
+        try{
+            const animaisResponse = await fetch(url)
+            const animaisJSON = await animaisResponse.json()
+            // console.log(animaisJSON)
+            animaisJSON.forEach((animal) => {
+                const divAnimal = createAnimal(animal)
+                // console.log(divAnimal)
+                const numerosGrid = document.querySelector('.numeros-grid')
+                numerosGrid.appendChild(divAnimal)
+            })
+            initAnimaNumeros()
+        }catch(erro){
+            console.log(erro)
+        }
+            
+    }
+    fetchAnimais('./animaisapi.json')
+    
+    function createAnimal(animal) {
+        // console.log(animal)
+        const div = document.createElement('div')
+        div.classList.add('numero-animal')
+        div.innerHTML = `<h3>${animal.especie}</h3><span data-numero>${animal.total}</span>`
+        // console.log(div)
+        return div
+    }
+}
+
