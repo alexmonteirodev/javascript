63e9c8f624442a467da1e25c9f276ee379b15d7c
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-04-05 01:34:07 +0200

    ex068-linha-de-comando-terminal

---
+++ origamid/exercicios/ex068-linha-de-comando-terminal/script.js
@@ -0,0 +1,54 @@
+
+                                                        // Automação Front End
+    // Linha de comando (CLI)
+        // É uma forma de interagir com o Computador através de texto. (no terminal)
+        
+        // Existem diferentes linhas de comando, a principal diferença está entre linhas de comando baseada em sistema $ (Unix) e > (Win)
+        // UNIX (Mac e Linux) vs Win (Windows)
+
+        // As linhas de comando mais comuns do Unix são Bash e Zsh e do windows são CMD e PowerShell:
+        // Bash e Zsh (Unix) | CMD e PowerShell (Windows)
+
+        // https://ss64.com - esse site contém todos os atalhos das linhas de comando
+//-----------------------------------------------------------------------------------------------------
+
+    // Atalhos
+        // Movimentar (Bash)
+
+            //$ cd sites - Move para a pasta sites
+
+            // $ cd .. - Move para a pasta anterior
+
+            // $ cd ~/Desktop/sites - Move para a pasta sites dentro de Desktop
+
+            // $ cd ~ - Move para o diretório principal do sistema / home
+
+            // $ clear - Limpa a tela
+
+
+        // Listar e Criar (Bash)
+
+            // $ ls - Lista diretórios e arquivos
+            
+            // $ ls -all - Lista diretórios, arquivos, invisíveis e detalhes
+            
+            // $ mkdir site - Cria o diretório site
+            
+            // $ touch index.html - Cria o arquivo index.html
+
+
+        // Remover (Bash) Obs: cuidado com esse comando pq ele deleta do pc e não tem como recuperar, sempre verificar se está na pasta certa para não apagar nada errado. Se apagar pasta essencial do funcionamento no computar, o pc para de funcionar.
+
+            // $ rm index.html - Remove o arquivo index.html
+
+            // $ rm -r site - Remove o diretório site e todos os arquivos dentro dele
+
+
+        // Dicas:
+            // seta para cima / baixo - Acessa o comando anterior
+
+            // tab - Auto-completa o comando
+
+            //obs: / significa a raiz do pc e o ~ significa diretório home
+
+            //comando 'sudo' quer dizer que quer usar um comando como um super usuário. Esse comando é específico de mac, portanto se quiser usar um comando e o mac não deixar, basta por o comando e usar o comando sudo que o mac vai pedir a senha do pc e vai fazer.
