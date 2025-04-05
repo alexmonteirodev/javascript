85d90f36c9d665620540a6644c0b36aff5a1fa6c
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-04-06 01:29:49 +0200

    ex073-git part 1

---
+++ origamid/exercicios/ex073-git/aula.js
@@ -0,0 +1,31 @@
+                                                        // Automação Front End
+    // Git
+        // Sistema de controle de versões. Facilita o trabalho em equipe e o controle de mudanças entre arquivos e diretórios.
+
+    // Github                                                    
+        // Plataforma online de hospedagem para repositórios Git. Existem outras como GitLab e Bitbucket. (no github o repositorio precisa ser de codigo aberto, logo, todos podem ver seu codigo, caso queira um codigo privao tem como mas tem que pagar, o bitbucket tem como ter privado de graça)
+//-----------------------------------------------------------------------------------------------------
+
+    // Git Setup
+
+        // Instalar o Git - https://git-scm.com/
+        
+        // Configurar Nome - $ git config --global user.name "Seu Nome"
+        
+        // Configurar Email - $ git config --global user.email "email@gmail.com"
+//-----------------------------------------------------------------------------------------------------
+
+    // Git Comandos
+
+        // $ git init - Inicia um repositório
+
+        // $ git add style.css - Adiciona o arquivo style.css ao index do git. Com o $ git add -A, adicionamos todos os arquivos.
+
+        // $ git status - Mostra os arquivos que tiveram mudanças. 
+        
+        // $ git commit -m 'Descrição' - Irá fazer fazer o commit do código adicionado com uma mensagem.
+
+
+
+                                           
+
