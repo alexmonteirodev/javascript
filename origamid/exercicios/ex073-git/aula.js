8e8a6389f726a747b74d8cb528d3e18764a0f8c6
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-04-06 02:11:05 +0200

    ex073-git part 2

--- origamid/exercicios/ex073-git/aula.js
+++ origamid/exercicios/ex073-git/aula.js
@@ -24,6 +24,46 @@
         // $ git status - Mostra os arquivos que tiveram mudanças. 
         
         // $ git commit -m 'Descrição' - Irá fazer fazer o commit do código adicionado com uma mensagem.
+//-----------------------------------------------------------------------------------------------------
+
+    // Branching
+        // Branch - Uma das principais vantagens do git é a possibilidade de criarmos 'ramificações'. Assim podemos trabalhar em funcionalidades adicionais para um projeto, sem modificarmos o 'ramificação princpal', o master.
+
+        // o tronco da arvore é a main/master e o galho é a branch, se usa o branch para quando for trabalhar em equipe todo mundo não mexer em várias coisas e bagunçar tudo, logo, se cria uma branch, ela passa por aprovação e depois se une ao tronco/main/master por meio de um merge.
+
+        // comandos:
+            
+            // $ git branch nomebranch - Toda vez que formos adicionar uma nova funcionalidade, devemos iniciar criando um novo branch ao invés de fazermos alterações direto no master. O que for modificado no branch não afetara o master.
+
+            // $ git checkout nomebranch - Irá mudar de branch. Podemos usar o atalho $git checkout -b novobranch, assim ele cria e muda de branch ao mesmo tempo.
+
+            // $ git branch - Mostra os branch's criados.
+//-----------------------------------------------------------------------------------------------------
+
+    // Workflow   
+        // Sempre crie um branch - Toda funcionalidade nova, crie um branch para desenvolver a mesma. $ git checkout -b feature1
+
+        // Após o desenvolvimento e commit, vá até o master e veja se existem mudanças - $ git checkout master e $ git pull
+
+        // Volte para o branch e dê um merge com o master - $ git checkout feature1 e depois $ git merge master
+
+        //Conflitos - Se existirem conflitos você será avisado e deverá lidar com os mesmos
+
+        // Git Push - Após lidar com os conflitos faça o push do branch: $ git push e $ git push --set-upstream origin feature1.
+
+
+        // um exemplo prático de como seria na pratica é:
+
+            // a pessoa que for trabalhar com vc para pegar o projeto a primeira coisa que tem que fazer é clonar o repositório (git clone https://enderecodorepositorio), depois usa o cd e vai pra pasta clonada e trabalha.
+            
+            //depois de trabalhar o ideal é ir na master para ver se tem alguma atualização (obs: se usar o git status ele não vai puxar pq a alteração é online), para atualizar o master e ver se teve alguma alteração tem que usar o git pull.
+
+            // basicamente, vc cria uma branch (checkout), trabalha no código, adiciona o codigo (git add -A) à branch, faz o commit (git commit -m 'alterei x coisa') para identificar a alteração, git status (para conferir) e depois com a autorização do manager faz o merge, ou seja, junta a branch (galho) na master (tronco), usando o 'git merge master' (estando na pasta da branch). Depois de conferir se não teve nenhuma divergencia entre o seu arquivo que fez o merge com o da master (o vs code mostra caso exista divergencia) aí se faz o git push (push é para subir o arquivo online)
+
+            // obs: no final geralmente o pessoal faz o merge e o pull e depois exclui a branch porque aquilo já foi resolvio e pra não ficar um monte de branchs. a pessoa que fez o commit continua tendo a branch na maquina e para deletar da maquina tbm basta usar o 'git branch -d nomedabranch'
+
+            //depois de tudo ao fazer o pull da master já virá o projeto com as alterações feitas.
+
 
 
 
