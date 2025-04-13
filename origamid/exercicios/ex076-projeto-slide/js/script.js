4eb667c5bea7d89ad1b2e336b3c070cb24c66667
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-04-13 16:13:27 +0200

    ex076 - navegacao btns prev e next

--- origamid/exercicios/ex076-projeto-slide/js/script.js
+++ origamid/exercicios/ex076-projeto-slide/js/script.js
@@ -1,3 +1,4 @@
-import Slide from "./slide.js";
-const slide = new Slide(".slide", ".slide-wrapper");
+import { Slide, SlideNav } from "./slide.js";
+const slide = new SlideNav(".slide", ".slide-wrapper");
 slide.init();
+slide.addArrow(".prev", ".next");
